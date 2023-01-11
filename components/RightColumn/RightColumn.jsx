e7f0d9c7a0b6354c1b4461baa5924b005c3bdd6c
Stavros Melidoniotis <melidon.stavros@gmail.com>
2023-01-11 18:07:52 +0200

    Changed RightColumn max-width on xl screens

--- components/RightColumn/RightColumn.jsx
+++ components/RightColumn/RightColumn.jsx
@@ -5,7 +5,7 @@ import Works from "../Works/Works";
 
 const RightColumn = ({ featuredProjects, snippets }) => {
   return (
-    <div className="relative lg:absolute lg:right-10 lg:top-0 lg:w-[736px] lg:max-w-xl xl:max-w-3xl mt-8 xl:mt-16 flex flex-col gap-20">
+    <div className="relative lg:absolute lg:right-10 lg:top-0 lg:w-[736px] lg:max-w-xl xl:max-w-2xl mt-8 xl:mt-16 flex flex-col gap-20">
       <FeaturedProjects projects={featuredProjects} />
       <AboutMe />
       <Works />
